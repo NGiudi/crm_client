a620d37e7da46b4ef6623d2548d824aea86cfd56
jeromiguez <jeromiguez@gmail.com>
2023-05-24 01:23:58 -0300

    Se agregaron import de axios y param de salesService

--- src/services/axios/salesService.js
+++ src/services/axios/salesService.js
@@ -3,3 +3,9 @@ import axios from "axios";
 const axiosClient = axios.create({
 	baseURL: `${import.meta.env.VITE_API_URL}`,
 });
+
+/**
+  Se hace la request al backend para obtener una página de ventas.
+
+	@param {object} queryObj objecto que contiene la página que se quiere traer.
+*/
