82ba3e3123bafaeb925c0301ecf1c651eaca2d58
jeromiguez <jeromiguez@gmail.com>
2023-05-25 23:23:57 -0300

    Se agrego modificaciones SalesList y se creo salesService, para mostrar la vista de ventas

--- src/services/axios/salesService.js
+++ src/services/axios/salesService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { LS_KEYS } from "../../assets/constants/constants";
+
 const axiosClient = axios.create({
 	baseURL: `${import.meta.env.VITE_API_URL}`,
 });
@@ -9,3 +11,27 @@ const axiosClient = axios.create({
 
 	@param {object} queryObj objecto que contiene la página que se quiere traer.
 */
+export const getSales = (queryObj) => {
+	const userToken = localStorage.getItem(LS_KEYS.userToken);
+  
+	const queryString = new  URLSearchParams(queryObj).toString();
+  
+	  return axiosClient.get(`/sales?${queryString}`, {
+	  headers: { "Authorization": userToken},
+	})   
+	  .then((res) => res.data);
+  };
+
+/**
+  Se hace la request al backend para obtener el detalle completo de ventas.
+
+	@param {number} id id del producto a buscar.
+*/
+export const getSale = (id) => {
+	const userToken = localStorage.getItem(LS_KEYS.userToken);
+  
+	return axiosClient.get(`/sales/${id}`, {
+	  headers: { "Authorization": userToken},
+	})
+	  .then((res) => res.data);
+  };
