3f88bfa7e20d32afd7fa38f7534e849b66714514
ManuelCastanon <manucasta01@gmail.com>
2023-06-03 00:12:51 -0300

    validacion new podruct

--- src/pages/products/ProductNewPage.vue
+++ src/pages/products/ProductNewPage.vue
@@ -5,43 +5,62 @@ import { PATHS } from "../../assets/constants/constants";
 </script>
 <template>
   <AppLayout>
-
+    <form class="needs-validation" novalidate @submit.prevent="submitForm">
       <div class="mb-3">
         <label>Marca</label>
-        <input v-model="newProduct.brand_name" class="form-control" />
+        <input v-model="newProduct.brand_name" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Nombre</label>
-        <input v-model="newProduct.name" class="form-control" />
+        <input v-model="newProduct.name" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div>
  
       <div class="mb-3">
         <label>Descripcion</label>
-        <input v-model="newProduct.description" class="form-control" />
+        <input v-model="newProduct.description" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Precio</label>
-        <input type="number" v-model="newProduct.price" class="form-control" />
+        <input type="number" v-model="newProduct.price" min="0" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo inválido
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Stock</label>
-        <input type="number" v-model="newProduct.stock" class="form-control" />
+        <input type="number" v-model="newProduct.stock" min="0" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo inválido
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Descuento</label>
-        <input  v-model="newProduct.discount" class="form-control" />
+        <input  v-model="newProduct.discount" min="0" max="100" class="form-control" />
+        <div class="invalid-feedback">
+            Campo inválido
+        </div>
       </div>
  
 
       <div class="mt-5 text-end">
-        <button class="button button-solid" @click="handleSubmit">
+        <button class="button button-solid" type="submit">
           Crear producto
         </button>
       </div>
+    </form>
   </AppLayout>
 </template>
 
@@ -53,10 +72,13 @@ import { PATHS } from "../../assets/constants/constants";
       };
     },
     methods: {
-      handleSubmit(){
+      handleSubmit(e){
+      if (e.target.checkValidity()) {        
         console.log(this.newProduct)
         createProduct(this.newProduct)
         .then (resp => (this.$router.push(`${PATHS.productsList}/${resp.id}`)))
+      }
+      e.target.classList.add("was-validated");      
       }
     }
   }
