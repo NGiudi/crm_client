d9670574f5c637f08ee11a526a8642d44950a7d4
ManuelCastanon <manucasta01@gmail.com>
2023-06-03 06:16:54 -0300

    validacion usuario

--- src/pages/users/UserNewPage.vue
+++ src/pages/users/UserNewPage.vue
@@ -11,34 +11,53 @@
 
 <template>
   <AppLayout>
+    <form class="needs-validation" novalidate @submit.prevent="handleSubmit">
       <div class="mb-3">
         <label>Nombre</label>
-        <input v-model="newUser.names" class="form-control" />
+        <input v-model="newUser.names" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Apellido</label>
-        <input v-model="newUser.last_name" class="form-control" />
+        <input v-model="newUser.last_name" required class="form-control" />
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Teléfono</label>
-        <input type="phone" v-model="newUser.phone" class="form-control" />
+        <input type="tel" v-model="newUser.phone" required class="form-control" pattern="[0-9]*" minlength="8" maxlength="20"/>
+        <div class="invalid-feedback">
+            <spam v-if="!newUser.phone"> Campo obligatorio </spam>
+            <spam v-else> Ingrese un teléfono válido </spam>
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Mail</label>
-        <input type="email" v-model="newUser.email" class="form-control" />
+        <input type="email" v-model="newUser.email" required class="form-control" />
+        <div class="invalid-feedback">
+            <spam v-if="!newUser.email"> Campo obligatorio </spam>
+            <spam v-else> Campo inválido </spam>
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Contraseña</label>
-        <input min="4" v-model="newUser.password" class="form-control" />
+        <input minlength="6" v-model="newUser.password" required class="form-control" />
+        <div class="invalid-feedback">
+            <spam v-if="!newUser.password"> Campo obligatorio </spam>
+            <spam v-else> Debe tener más de 6 caracteres </spam>
+        </div>
       </div>      
 
       <div class="mb-3">
         <label>Rol</label>
-        <select v-model="newUser.role" class="form-control">
+        <select v-model="newUser.role" required class="form-control">
           <option value="seller">
             Vendedor
           </option>
@@ -46,11 +65,14 @@
             Administrador
           </option>
         </select>
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Estado</label>
-        <select v-model="newUser.active" class="form-control">
+        <select v-model="newUser.active" required class="form-control">
           <option :value=true>
             Activo
           </option>
@@ -58,13 +80,17 @@
             Inactivo
           </option>
         </select>
+        <div class="invalid-feedback">
+            Campo obligatorio
+        </div>
       </div> 
 
       <div class="mt-5 text-end">
-        <button class="button button-solid" @click="handleSubmit">
+        <button class="button button-solid" type="submit">
           Crear usuario
         </button>
       </div>
+    </form>
   </AppLayout>
 </template>
 
@@ -84,10 +110,12 @@
       };
     },
     methods: {
-      handleSubmit(){
-        console.log(this.newUser)
+      handleSubmit(e){
+      if (e.target.checkValidity()) {  
         createUser(this.newUser)
         .then (resp => (this.$router.push(`${PATHS.usersList}/${resp.id}`)))
+      }
+      e.target.classList.add("was-validated");      
       }
     },
   }
